b35a2c6b1444e3ee64b8a32a6a56600b9998f193
degric <ricardodegante48@gmail.com>
2022-08-16 21:19:04 -0500

    First Commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def inicio():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(port=5000)
